cdb94837296d78ff276774ae756114ebc0f29e5a
Emily_Richard <richard.emily.e@gmail.com>
2019-10-04 14:07:44 -0400

    fixes

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-import * as rtl from 'react-testing-library';
+import { render } from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('App renders without crashing.', () => {
+  render(<App />);
 });
 
-test('app has header', () => {
-  const app = render(<App />);
-  app.getByText(/Players by Search Count/i);
+test('App has a header title.', () => {
+  const {getByText} = render(<App />);
+  getByText(/players/i);
 })
 
 // test('styles change to darkmode when toggled', () => {
